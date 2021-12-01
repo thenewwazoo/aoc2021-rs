60f0715835c4f68964a1a12ab068d3a71588fda6
Brandon Matthews <bmatthews@linkedin.com>
2021-12-01 14:06:14 -0800

    Reindent

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -8,40 +8,34 @@ pub fn main() {
 pub fn part1() -> usize {
     find_increases(
         &read_lines_from("input/day1.txt")
-        .unwrap()
-        .map(|l| l.unwrap().parse::<u64>().expect("not a number"))
-        .collect::<Vec<u64>>()
+            .unwrap()
+            .map(|l| l.unwrap().parse::<u64>().expect("not a number"))
+            .collect::<Vec<u64>>(),
     )
 }
 
 pub fn part2() -> usize {
-    find_increases(
-        &group_lines(
-            &read_lines_from("input/day1.txt")
-                .unwrap()
-                .map(|l| l.unwrap().parse::<u64>().expect("not a number"))
-                .collect::<Vec<u64>>()
-        )
-    )
+    find_increases(&group_lines(
+        &read_lines_from("input/day1.txt")
+            .unwrap()
+            .map(|l| l.unwrap().parse::<u64>().expect("not a number"))
+            .collect::<Vec<u64>>(),
+    ))
 }
 
 fn find_increases(m: &[u64]) -> usize {
-    m
-        .windows(2)
+    m.windows(2)
         .map(|w| match w {
             &[] => false,
             &[_] => false,
             [a, b, ..] => b > a,
         })
-    .filter(|&a| a)
+        .filter(|&a| a)
         .count()
 }
 
 fn group_lines(m: &[u64]) -> Vec<u64> {
-    m
-        .windows(3)
-        .map(|w| w.iter().sum())
-        .collect()
+    m.windows(3).map(|w| w.iter().sum()).collect()
 }
 
 #[cfg(test)]
@@ -49,8 +43,7 @@ mod tests {
 
     use super::*;
 
-    const TEST_DATA_STR: &str =
-        &"199\n\
+    const TEST_DATA_STR: &str = &"199\n\
           200\n\
           208\n\
           210\n\
@@ -70,16 +63,12 @@ mod tests {
         assert_eq!(find_increases(&test_data), 7);
     }
 
-
     #[test]
     fn test_find_increases_multi() {
         let test_data = TEST_DATA_STR
             .lines()
             .map(|l| l.parse::<u64>().expect("not a number"))
             .collect::<Vec<u64>>();
-        assert_eq!(
-            find_increases(&group_lines(&test_data)),
-            5
-        );
+        assert_eq!(find_increases(&group_lines(&test_data)), 5);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,7 @@
-use std::io::Error;
 use std::fs::File;
+use std::io::Error;
 use std::io::{BufRead, BufReader, Lines};
 
 pub fn read_lines_from(input_fn: &str) -> Result<Lines<BufReader<File>>, Error> {
-    Ok(
-        BufReader::new(File::open(input_fn)?).lines()
-    )
+    Ok(BufReader::new(File::open(input_fn)?).lines())
 }
